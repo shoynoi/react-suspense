7814e9ef2da46151e92710b67c9169ebbe52a4fd
shoynoi <shoynoi.jp@gmail.com>
2023-08-13 12:07:10 +0900

    exercise 5
    - 画像の読み込みをSuspendさせる
      - ネットワークが遅い場合、データのみが先に表示されて画像が後から表示されたたり、別の画像を表示させる時に以前の画像が表示されたままになったりする問題が発生する
      - 画像の読み込みもpromise を throw させてSuspendさせることでデータの表示と画像の表示のタイミングを一致させることができ、ユーザー体験の向上に繋がる

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -9,33 +9,25 @@ import {
   PokemonDataView,
   PokemonErrorBoundary,
 } from '../pokemon'
-import {createResource} from '../utils'
+import {createResource, preloadImage} from '../utils'
 
-// ❗❗❗❗
-// 🦉 On this one, make sure that you UNCHECK the "Disable cache" checkbox
-// in your DevTools "Network Tab". We're relying on that cache for this
-// approach to work!
-// ❗❗❗❗
+const imgSrcResourceCache = {}
 
-// we need to make a place to store the resources outside of render so
-// 🐨 create "cache" object here.
-
-// 🐨 create an Img component that renders a regular <img /> and accepts a src
-// prop and forwards on any remaining props.
-// 🐨 The first thing you do in this component is check whether your
-// imgSrcResourceCache already has a resource for the given src prop. If it does
-// not, then you need to create one (💰 using createResource).
-// 🐨 Once you have the resource, then render the <img />.
-// 💰 Here's what rendering the <img /> should look like:
-// <img src={imgSrcResource.read()} {...props} />
+function Img({src, ...props}) {
+  let imgSrcResource = imgSrcResourceCache[src]
+  if (!imgSrcResource) {
+    imgSrcResource = createResource(preloadImage(src))
+    imgSrcResourceCache[src] = imgSrcResource
+  }
+  return <img src={imgSrcResource.read()} {...props} />
+}
 
 function PokemonInfo({pokemonResource}) {
   const pokemon = pokemonResource.read()
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
-        {/* 🐨 swap this img for your new Img component */}
-        <img src={pokemon.image} alt={pokemon.name} />
+        <Img src={pokemon.image} alt={pokemon.name} />
       </div>
       <PokemonDataView pokemon={pokemon} />
     </div>
